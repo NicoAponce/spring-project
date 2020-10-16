5295fb5a9a4f84277ccc19b87798e9faea750303
nicolas <aponcenicolas@gmail.com>
2020-10-16 14:54:20 -0500

    add

--- api-rest-shopping/src/main/java/pe/spring/project/shopping/ApiRestShoppingApplication.java
+++ api-rest-shopping/src/main/java/pe/spring/project/shopping/ApiRestShoppingApplication.java
@@ -2,7 +2,9 @@ package pe.spring.project.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiRestShoppingApplication {
 
